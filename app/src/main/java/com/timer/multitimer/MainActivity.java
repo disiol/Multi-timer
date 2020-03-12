a6d1e409c2aa6dcf0790d2dd835b58178f05e548
denis <025896321Denis>
2020-03-12 16:58:22 +0200

    my log

--- app/src/main/java/com/timer/multitimer/MainActivity.java
+++ app/src/main/java/com/timer/multitimer/MainActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.timer.multitimer.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,18 +27,22 @@ public class MainActivity extends AppCompatActivity {
     private String hoursTextViewString;
     private String minutesTextViewS;
     private String secondsTextViewS;
+    private List<TimerModel> timerModels = new ArrayList<>();
+    private SharedPreferences preferences;
+    private static final String TIMES_LIST = "times list";
+    public static final String MYLOG_TEG = "my log";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        preferences = getPreferences(MODE_PRIVATE);
+
         binding.startButton.setOnClickListener(v -> {
 
 
-
-
             getDataForTimer();
-
 
             setTimer(timerTime);
             countDownTimer.start();
@@ -63,36 +71,61 @@ public class MainActivity extends AppCompatActivity {
 
 
         binding.addNewTimerButton.setOnClickListener(v -> {
-            TextView hoursTextView = new TextView(this);
-            binding.newTimerLinearLayout.addView(hoursTextView);
-            hoursTextView.setText(String.format("%s:%s:%s"));
-            setTimer(timerTime);
+            if (timerModels.size() > 0) {
+                adTimerToList();
+            }else{
+                getDataForTimer();
+                adTimerToList();
+            }
 
         });
     }
 
+    private void adTimerToList() {
+        String timerTitle = timerModels.get(timerModels.size() - 1).getTimerTitle();
+        String hoursTextViewString = timerModels.get(timerModels.size() - 1).getHoursTextViewString();
+        String minutesTextViewS = timerModels.get(timerModels.size() - 1).getMinutesTextViewS();
+        String secondsTextViewS = timerModels.get(timerModels.size() - 1).getSecondsTextViewS();
+
+        TextView timerTitleextView = new TextView(this);
+        TextView hoursTextView = new TextView(this);
+        binding.timersList.addView(timerTitleextView);
+        binding.timersList.addView(hoursTextView);
+
+        timerTitleextView.setText(timerTitle);
+        hoursTextView.setText(String.format("%s:%s:%s", hoursTextViewString, minutesTextViewS, secondsTextViewS));
+    }
+
     private void getDataForTimer() {
-         hoursTextViewString = binding.hoursTextView.getText().toString();
+        hoursTextViewString = binding.hoursTextView.getText().toString();
         if (!hoursTextViewString.isEmpty()) {
             int hours = Integer.parseInt(hoursTextViewString) * 3600000;
             timerTime = timerTime + hours;
         }
 
-         minutesTextViewS = binding.minutesTextView.getText().toString();
+        minutesTextViewS = binding.minutesTextView.getText().toString();
         if (!minutesTextViewS.isEmpty()) {
             int minutes = Integer.parseInt(minutesTextViewS) * 60000;
             timerTime = timerTime + minutes;
 
         }
 
-         secondsTextViewS = binding.secondsTextView.getText().toString();
+        secondsTextViewS = binding.secondsTextView.getText().toString();
         if (!secondsTextViewS.isEmpty()) {
             int secs = Integer.parseInt(secondsTextViewS) * 1000;
             timerTime = timerTime + secs;
 
         }
-    }
 
+        String timerTitle = binding.timerTitleTextView.getText().toString();
+
+        timerModels.add(new TimerModel(hoursTextViewString, minutesTextViewS, secondsTextViewS, timerTime, timerTitle));
+
+        Log.d(MYLOG_TEG, "timerModels "  + timerModels.get(timerModels.size() -1));
+        Log.d(MYLOG_TEG, "timerModels.size() "  + timerModels.size());
+        //preferences.edit().putString(TIMES_LIST, String.valueOf(timerModels));
+
+    }
 
 
     private void setTimer(long time) {
--- app/src/main/java/com/timer/multitimer/TimerModel.java
+++ app/src/main/java/com/timer/multitimer/TimerModel.java
@@ -2,13 +2,49 @@ package com.timer.multitimer;
 
 public class TimerModel {
 
+    private final String timerTitle;
     private String hoursTextViewString;
     private String minutesTextViewS;
     private String secondsTextViewS;
+    private long timerTime;
 
-    public TimerModel(String hoursTextViewString, String minutesTextViewS, String secondsTextViewS) {
+
+    public TimerModel(String hoursTextViewString, String minutesTextViewS, String secondsTextViewS, long timerTime, String timerTitle) {
         this.hoursTextViewString = hoursTextViewString;
         this.minutesTextViewS = minutesTextViewS;
         this.secondsTextViewS = secondsTextViewS;
+        this.timerTime = timerTime;
+        this.timerTitle = timerTitle;
+    }
+
+    public String getHoursTextViewString() {
+        return hoursTextViewString;
+    }
+
+    public String getMinutesTextViewS() {
+        return minutesTextViewS;
+    }
+
+    public String getSecondsTextViewS() {
+        return secondsTextViewS;
+    }
+
+    public long getTimerTime() {
+        return timerTime;
+    }
+
+    public String getTimerTitle() {
+        return timerTitle;
+    }
+
+    @Override
+    public String toString() {
+        return "TimerModel{" +
+                "timerTitle='" + timerTitle + '\'' +
+                ", hoursTextViewString='" + hoursTextViewString + '\'' +
+                ", minutesTextViewS='" + minutesTextViewS + '\'' +
+                ", secondsTextViewS='" + secondsTextViewS + '\'' +
+                ", timerTime=" + timerTime +
+                '}';
     }
 }
